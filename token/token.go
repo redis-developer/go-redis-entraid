9d9d71a65329bfd8ce89c9e50431fc99ac957c28
Nedyalko Dyakov <nedyalko.dyakov@gmail.com>
2025-04-11 00:50:07 +0300

    reorder file

--- token/token.go
+++ token/token.go
@@ -6,11 +6,24 @@ import (
 	"github.com/redis/go-redis/v9/auth"
 )
 
-// Token represents the authentication token used to access the Entraid API.
-// It contains the username, password, expiration time, time to live, and the raw token.
-// The token is used to authenticate the user and authorize access to the API.
-// The token is typically obtained from an identity provider and is used to access the Entraid API.
-// The token is valid for a limited time and must be refreshed periodically.
+// Ensure Token implements the auth.Credentials interface.
+var _ auth.Credentials = (*Token)(nil)
+
+// New creates a new token with the specified username, password, raw token, expiration time, received at time, and time to live.
+// NOTE: This won't do any validation on the token, expiresOn, receivedAt, or ttl. It will simply create a new token instance.
+func New(username, password, rawToken string, expiresOn, receivedAt time.Time, ttl int64) *Token {
+	return &Token{
+		username:   username,
+		password:   password,
+		expiresOn:  expiresOn,
+		receivedAt: receivedAt,
+		ttl:        ttl,
+		rawToken:   rawToken,
+	}
+}
+
+// Token represents parsed authentication token used to access the Redis server.
+// It implements the auth.Credentials interface.
 type Token struct {
 	// username is the username of the user.
 	username string
@@ -27,13 +40,11 @@ type Token struct {
 }
 
 // BasicAuth returns the username and password for basic authentication.
-// It implements the auth.Credentials interface.
 func (t *Token) BasicAuth() (string, string) {
 	return t.username, t.password
 }
 
 // RawCredentials returns the raw credentials for authentication.
-// It implements the auth.Credentials interface.
 func (t *Token) RawCredentials() string {
 	return t.rawToken
 }
@@ -43,31 +54,9 @@ func (t *Token) ExpirationOn() time.Time {
 	return t.expiresOn
 }
 
-// Token implements the auth.Credentials interface.
-var _ auth.Credentials = (*Token)(nil)
-
-// New creates a new token with the specified username, password, raw token, expiration time, received at time, and time to live.
-func New(username, password, rawToken string, expiresOn, receivedAt time.Time, ttl int64) *Token {
-	return &Token{
-		username:   username,
-		password:   password,
-		expiresOn:  expiresOn,
-		receivedAt: receivedAt,
-		ttl:        ttl,
-		rawToken:   rawToken,
-	}
-}
-
+// Copy creates a copy of the token.
 func (t *Token) Copy() *Token {
 	return copyToken(t)
-}
-
-// copyToken creates a copy of the token.
-func copyToken(token *Token) *Token {
-	if token == nil {
-		return nil
-	}
-	return New(token.username, token.password, token.rawToken, token.expiresOn, token.receivedAt, token.ttl)
 }
 
 // compareCredentials two tokens if they are the same credentials
@@ -83,4 +72,12 @@ func (t *Token) compareRawCredentials(token *Token) bool {
 // compareToken compares two tokens if they are the same token
 func (t *Token) compareToken(token *Token) bool {
 	return t.compareCredentials(token) && t.compareRawCredentials(token)
+}
+
+// copyToken creates a copy of the token.
+func copyToken(token *Token) *Token {
+	if token == nil {
+		return nil
+	}
+	return New(token.username, token.password, token.rawToken, token.expiresOn, token.receivedAt, token.ttl)
 }
